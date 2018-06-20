5ad59b3bdbcc45921bac681a47867dd4f0189516
BNgoum <benjamin.ngoum@gmail.com>
2018-06-20 16:41:05 +0200

    Fix input text

--- src/client/components/InputText/InputText.jsx
+++ src/client/components/InputText/InputText.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 
 const InputText = ({ input, label, disabled, type, meta: { touched, error, warning } }) => (
   <div className="wrapper-input-text">
-    <input type="text" placeholder={label} />
     <input {...input} placeholder={label} type={type} disabled={ disabled }/>
     <span>{label}</span>
     {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
